20aa2e23e746f0490c9d9792fbd7f044fcac02cc
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2020-05-13 03:36:53 +0900

    [UPDATE] 패턴검사 코드 모듈화
    가독성 개선

--- test/test.py
+++ test/test.py
@@ -11,34 +11,41 @@ while True:
     if not line: break
     data = line
 
-    # 주민번호 탐지 (숫자6자리 + "-" + 숫자7자리)
-    pat1 = re.compile("(?P<birth>\d{6})[-]\d{7}")
-    m1 = pat1.search(data)
-    if m1:
-        mask1 = input("주민등록번호 패턴이 탐지되었습니다. "+m1.group()+"\n주민등록번호를 마스킹하시겠습니까? (Y/N)\n")
-        if mask1=='y':
-            print(pat1.sub("\g<birth>-*******", data))
-        else:
-            print(data)
+    # N 주민번호 탐지 (숫자6자리 + "-" + 숫자7자리)
+    Npat = re.compile("(?P<birth>\d{6})[-]\d{7}")
+    Nm = Npat.search(data)
 
     # P 여권번호 탐지 (신 전자 여권번호 (M1234567형태))
-    pat2 = re.compile("(?P<Ptype>[a-zA-Z])\d{7}")
-    m2 = pat2.search(data)
-    if m2:
-        mask2 = input("여권번호 패턴이 탐지되었습니다. " + m2.group()+"\n여권번호를 마스킹하시겠습니까? (Y/N)\n")
-        if mask2=='y':
-            print(pat2.sub("\g<Ptype>*******", data))
-        else:
-            print(data)
+    Ppat = re.compile("(?P<Ptype>[a-zA-Z])\d{7}")
+    Pm = Ppat.search(data)
 
     # D 운전면허번호 탐지
-    pat3 = re.compile("(\d{2})[-]\d{2}[-]\d{6}[-]\d{2}")
-    m3 = pat3.search(data)
-    if m3:
-        mask3 = input("운전면허번호 패턴이 탐지되었습니다. "+m3.group()+"\n운전면허번호를 마스킹하시겠습니까? (Y/N)\n")
-        if mask3=='y':
-            print(pat3.sub("**-**-******-**", data))
+    Dpat = re.compile("(\d{2})[-]\d{2}[-]\d{6}[-]\d{2}")
+    Dm = Dpat.search(data)
+
+    
+    if Nm: # 주민번호 패턴일 때
+        mask = input("주민등록번호 패턴이 탐지되었습니다. "+Nm.group()+"\n주민등록번호를 마스킹하시겠습니까? (Y/N)\n")
+        if mask=='y':
+            print(Npat.sub("\g<birth>-*******", data))
+        else:
+            print(data)
+            
+    elif Pm: # 여권번호 패턴일 때
+        mask = input("여권번호 패턴이 탐지되었습니다. " + Pm.group()+"\n여권번호를 마스킹하시겠습니까? (Y/N)\n")
+        if mask=='y':
+            print(Ppat.sub("\g<Ptype>*******", data))
         else:
             print(data)
 
+    elif Dm: # 운전면허번호 패턴일 때
+        mask = input("운전면허번호 패턴이 탐지되었습니다. "+Dm.group()+"\n운전면허번호를 마스킹하시겠습니까? (Y/N)\n")
+        if mask=='y':
+            print(Dpat.sub("**-**-******-**", data))
+        else:
+            print(data)
+            
+
+            
+    
 f.close()

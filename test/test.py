25d75fb16ec515e5c90591c5010645b58ffcbfbb
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2020-05-13 01:01:12 +0900

    [UPDATE] test.py 여권번호 탐지 추가
    테스트 안한상태

--- test/test.py
+++ test/test.py
@@ -8,13 +8,22 @@ while True:
     if not line: break
     data = line
 
-    # 주민번호 탐지 
-    pat = re.compile("(\d{6})[-]\d{7}")
-    m = pat.search(data)
-    if m:
-        mask = input("주민번호가 탐지되었습니다. "+m.group()+"\n마스킹하시겠습니까? (Y/N)\n")
-        if mask=='y':
-            print(pat.sub("\g<1>-*******", data))
+    # 주민번호 탐지 (숫자6자리 + "-" + 숫자7자리)
+    pat1 = re.compile("(\d{6})[-]\d{7}")
+    m1 = pat1.search(data)
+    if m1:
+        mask1 = input("주민등록번호가 탐지되었습니다. "+m1.group()+"\n주민등록번호를 마스킹하시겠습니까? (Y/N)\n")
+        if mask1=='y':
+            print(pat1.sub("\g<1>-*******", data))
         else:
             print(data)
+
+    # 여권번호 탐지 (신 전자 여권번호 (M1234567형태))
+    pat2 = re.compile("[a-zA-Z]\d{7}")
+    m2 = pat2.search(data)
+    if m2:
+        mask2 = input("여권번호가 탐지되었습니다. " + m2.group()+"\n여권번호를 마스킹하시겠습니까? (Y/N)\n")
+        if mask2=='y':
+            print(pat.sub("\g<1>*******", data))
+
 f.close()
